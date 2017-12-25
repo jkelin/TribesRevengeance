e1f43efd3e2650d797332127bb507aedb4836ab8
me@fireant.pw <me@fireant.pw>
2017-12-25 21:49:45 +0100

    Disable instantclick for now

--- assets/main.js
+++ assets/main.js
@@ -1,28 +1,9 @@
-(function (){
-  document.addEventListener('DOMContentLoaded', function () {
-    // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-
-      // Add a click event on each of them
-      $navbarBurgers.forEach(function ($el) {
-        $el.addEventListener('click', function () {
-
-          // Get the target from the "data-target" attribute
-          var target = $el.dataset.target;
-          var $target = document.getElementById(target);
-
-          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-
-        });
-      });
-    }
-  });
-})();
+function subscribeLoad(fun) {
+  document.addEventListener('DOMContentLoaded', fun);
+  if(window.InstantClick) {
+    window.InstantClick.on('change', fun);
+  }
+}
 
 (function(){
   document.addEventListener('DOMContentLoaded', function () {
@@ -30,5 +11,31 @@
       window.InstantClick.init();
     }
   });
-})()
+})();
+
+subscribeLoad(function () {
+  // debugger;
+
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+});
 
